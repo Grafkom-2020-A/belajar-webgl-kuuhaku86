44036a6236d95a3682b6a6fd3c6f8b98bd01c0b4
Yohan Ardiansyah <yohan.ardiansyah90@gmail.com>
2020-10-12 08:48:00 +0700

    segitiga line loop

--- script.js
+++ script.js
@@ -2,22 +2,24 @@ function main() {
     let canvas = document.getElementById("myCanvas");
     let gl = canvas.getContext("webgl");
 
-    let vertexShaderCode = `
-        void main() {
-            gl_Position = vec4(0.0, 0.0, 0.0, 1.0);
-            gl_PointSize = 50.0;
-        }
-    `;
+    let vertices = [
+        -0.5, 0.5,      // Titik A 
+        -0.5, -0.5,     // Titik B
+        0.5, -0.5,       // Titik C
+    ];
+
+    let positionBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+    gl.bindBuffer(gl.ARRAY_BUFFER, null);
+
+    let vertexShaderCode = document.getElementById('vertexShaderSource').innerText;
 
     let vertexShader = gl.createShader(gl.VERTEX_SHADER);
     gl.shaderSource(vertexShader, vertexShaderCode);
     gl.compileShader(vertexShader);
 
-    let fragmentShaderCode = `
-        void main() {
-            gl_FragColor = vec4(1.0, 1.0, 0.0, 1.0);
-        }
-    `;
+    let fragmentShaderCode = document.getElementById('fragmentShaderSource').innerText;
 
     let fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
     gl.shaderSource(fragmentShader, fragmentShaderCode);
@@ -29,8 +31,17 @@ function main() {
     gl.linkProgram(shaderProgram);
     gl.useProgram(shaderProgram);
 
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+    let aPosition = gl.getAttribLocation(shaderProgram, "a_Position");
+    gl.vertexAttribPointer(aPosition, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(aPosition);
+
     gl.clearColor(0.2, 0.0, 0.0, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT);
 
-    gl.drawArrays(gl.POINTS, 0, 1)
+    let primitive = gl.LINE_LOOP;
+    let offset = 0;
+    let count = 3;
+
+    gl.drawArrays(primitive, offset, count)
 }
